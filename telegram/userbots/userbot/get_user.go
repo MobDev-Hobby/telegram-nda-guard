76a3005815e462e859060d0284e5e666b4d4ebe0
Andrey Khodzhakhanov <aakhodzhakhanov@avito.ru>
2024-10-11 16:58:34 +0500

    Fix npe in userbot on non-inited bot

--- telegram/userbots/userbot/get_user.go
+++ telegram/userbots/userbot/get_user.go
@@ -1,9 +1,17 @@
 package userbot
 
 func (d *Domain) UserID() int64 {
+	if d.userBot.me == nil {
+		d.log.Errorf("Can't get user bot ID")
+		return 0
+	}
 	return d.userBot.me.ID
 }
 
 func (d *Domain) Username() string {
+	if d.userBot.me == nil {
+		d.log.Errorf("Can't get user bot Username")
+		return ""
+	}
 	return d.userBot.me.Username
 }
